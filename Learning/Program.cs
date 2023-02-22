4276be807b6dc2e73c2e4244f23c83734204d4a0
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-02-22 13:15:22 +0100

    22-feb-2023

---
+++ Learning/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Hospital theHospital = new Hospital();
+
+theHospital.Environment = "Noisy";
+theHospital.people = "Doctors, Nurses and Patients";
+theHospital.Properties = "Wards";
+
+theHospital.Hospital_tour();
